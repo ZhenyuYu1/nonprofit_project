773123b04fc8b0d67d83c4369641997f8f3f0f04
Mission Bit <missionbit@36a-MissionBit.local>
2020-09-30 17:40:52 -0700

    trying to make while loop work

---
+++ nonprofitproject.py
@@ -0,0 +1,63 @@
+choice1 = 0;
+choice2 = 0;
+choice3 = 0;
+def displayIntro():
+  #put an Introduction message to the users
+    donate = input("Hello! Would you like to donate to a non-profit?(Y/N)")
+    if donate == "Y":
+        displayNonProfits()
+    elif donate == "N":
+        print("Have a nice day.")
+    else:
+        print("I don't understand.")
+
+def displayNonProfits():
+  #print all the non-profits to the screen numerically. For Example:
+#    1. World Central Kitchen
+#    2. Crisis Text Line
+#    3. Heart to Heart International
+    while donate = "Y":
+        print("1.World Central Kitchen")
+        print("2.Crisis Text Line")
+        print("3.Heart to Heart International")
+        choice = int(input("Enter the number of the non-profit you would like to donate to: "))
+        amount = int(input("Enter the amount of money you would like to donate: "))
+        if choice == 1:
+            choice1 += amount
+            print(f"The World Central Kitchen has received {amount} and now has a total of {choice1}")
+        elif choice == 2:
+            choice2 += amount
+            print(f"The Crisis Text Line has received {amount} and now has a total of {choice2}")
+        else:
+            choice3 += amount
+            print(f"The Heart to Heart Inter has received {amount} and now has a total of {choice3}")
+        donate = input("Would you like to donate to another non-profit?(Y/N)")
+        if donate == "Y":
+            displayNonProfits()
+            elif donate == "N":
+                print("Have a nice day.")
+            else:
+                print("I don't understand.")
+
+
+def main():
+    displayIntro()
+    while donate == "Y":
+        displayNonProfits()
+
+main()
+    
+    
+    
+	#steps: 
+	#1. welcome the user using a unique Intro Message (use a function for this)
+	#2. create a variable for each nonprofit/charity that keeps track of how much total money has been donated
+	#3. display all the non-profits to the user and ask which one they would like to donate to 
+	#4. then ask the user how much money they would like to donate to that specific charity chosen in step 3
+	#5. update the variable created in step 2 with whatever amount the user wanted to donated
+			#for example: missionbit = 0 -> user wants to donate 100 so you would add 100 to missionbit. missionbit is now 100
+	#6. now display all the nonprofits with their new total amounts donated
+	#7. repeat this process until the user wants to stop donating (only do this if you cover while loops)
+	#push to github after every single step
+          
+        
